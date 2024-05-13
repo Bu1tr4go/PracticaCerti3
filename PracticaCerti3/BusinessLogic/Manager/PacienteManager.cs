9d4508e9acbcbf0ffd86821a726cbef7eb0ae150
Daniel Torrico Buitrago <danieltorricob07@gmail.com>
2024-05-13 17:06:43 -0400

    P3-001 Commit implementando la funcionalidad de get que devuelve el codigo dado nombre, apellido y ci

---
+++ PracticaCerti3/BusinessLogic/Manager/PacienteManager.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Manager
+{
+    public class PacienteManager
+    {
+        public string CrearCodePaciente(Paciente paciente)
+        {
+            return $"{paciente.Nombre[0]}{paciente.Apellido[0]}{paciente.CI}";
+        }
+    }
+}
---
+++ PracticaCerti3/PracticaCerti3/Controllers/PacienteController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PracticaCerti3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        // GET: api/<PacienteController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PacienteController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet("{nombre}/{apellido}/{ci}")]
+        public string Get(string nombre, string apellido, int ci)
+        {
+            //Esta es la unica funcionalidad de la practica 3, por lo cual no se vio necesario implementar logs o error handling
+            return $"{nombre[0]}{apellido[0]}{ci}";
+        }
+
+        // POST api/<PacienteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PacienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PacienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
